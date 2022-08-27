58ab70c6b7f7b7e5d2d3934a42e1a23f756aacad
Zizan Ayub Al-Khalil <65456659+zizanayub@users.noreply.github.com>
2022-08-27 18:17:36 +0600

    Create PythonFundamentals.py

---
+++ PythonFundamentals.py
@@ -0,0 +1,40 @@
+##27.08.2022
+
+##Basic Input Output
+
+print ("Hello World")
+
+##Data types and variables
+
+text_to_print = "Hello World"
+print (text_to_print)
+
+
+##integer  data type
+
+number1 = 23
+number2 = 78
+print(number1+number2)
+
+
+##String Data Type
+
+s = "100"
+# print(s)
+s = 12
+print(s)
+
+#Here, the value of variable s has been changed. The print(s) function prints the second one. 
+
+
+#Input Function
+num1 = input("Please type an integer and press enter: ")
+num2 = input("Please type an integer and press enter: ")
+print(num1+num2)
+
+####Answer is 23. Because all of the inputs taken are in STRING. This is a default case. If we want to give int, just add 
+#'int' before the input!
+
+num1 = int (input())
+num2 = int (input())
+print(num1+num2)
