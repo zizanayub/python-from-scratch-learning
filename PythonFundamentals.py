6bd1e5ceb331ce626e27332c44b8bf9db7fbe0e7
Zizan Ayub Al-Khalil <65456659+zizanayub@users.noreply.github.com>
2022-09-10 17:54:16 +0600

    Update PythonFundamentals.py

--- PythonFundamentals.py
+++ PythonFundamentals.py
@@ -92,3 +92,20 @@ else:
     grade = "B"
 
 print("Your grade is: ",grade)
+
+
+
+
+
+
+
+
+
+#Leap Year
+year = int(input())
+if (year%4==0 and year%100!=0):
+    print("Leap Year!")
+elif (year%400 ==0):
+    print("Leap Year!")
+else:
+    print("Not a leap year!")
